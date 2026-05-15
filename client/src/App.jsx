92f974f83e1b9a254eb924fd82323003d483d9a1
Anshu Raj <anshu604125@gmail.com>
2026-05-16 00:04:10 +0530

    Update ServerUrl to production endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory.jsx'
 import Pricing from './pages/Pricing.jsx'
 import InterviewReport from './pages/InterviewReport.jsx'
 
-export const ServerUrl = "http://localhost:8000"
+export const ServerUrl = "https://resumetoroom-1.onrender.com"
 
 function App() {
 
